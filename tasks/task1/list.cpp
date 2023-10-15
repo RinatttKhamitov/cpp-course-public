f88c3ed392ffd1df2a9ea3a266ff01c4b570b37e
Ринат <stoneaz@mail.ru>
2023-10-15 19:38:29 +0300

    task1

--- tasks/task1/list.cpp
+++ tasks/task1/list.cpp
@@ -3,21 +3,26 @@
 
 List::List()
 {
-
+    first = nullptr;
+    last = nullptr;
 }
 
 
 bool
 List::is_empty()
 {
-
+    return (first == nullptr);
 }
 
 
 void
 List::clear()
 {
-
+    if(is_empty())
+        return;
+    while(first != nullptr){
+        remove_back();
+    }
 }
 
 
@@ -25,27 +30,62 @@ List::clear()
 void
 List::push_front(int _val)
 {
-
+    Node* newNode = new Node(_val);
+    if(is_empty()){
+        first = newNode;
+        last = newNode;
+        return;
+    }
+    newNode->next = first;
+    first = newNode;
 }
 
 
 void
 List::push_back(int _val)
 {
-
+    Node* newNode = new Node(_val);
+    if(is_empty()){
+        first = newNode;
+        last = newNode;
+        return;
+    }
+    last->next = newNode;
+    last = newNode;
 }
 
 
 Node*
 List::find(int _val)
 {
-
+    if (is_empty()){
+        return;
+    }
+    Node* current = first;
+    while (current != nullptr){
+        if(current->val == _val){
+            return current;
+        }
+        current = current->next;
+    }
+    return current;
 }
 
 
 void
 List::remove_front()
 {
+    if (is_empty()){
+        return;
+    }
+    if (first == last){
+        first = nullptr;
+        last = nullptr;
+        return;
+    }
+    Node* del = first;
+    first = first->next;
+    delete del;
 
 }
 
@@ -53,12 +93,44 @@ List::remove_front()
 void
 List::remove_back()
 {
+    if (is_empty())
+        return;
+    if(first == last){
+        first = nullptr;
+        last = nullptr;
+        return;
+    }
+    Node* current = first; 
+    
+    while (current->next->next != nullptr)
+        current = current->next;
 
+    current->next = nullptr;
+    last = current;
 }
 
 
 bool
 List::remove(const Node* _node)
 {
+    if(is_empty() || _node == nullptr || find(_node->val) == nullptr)
+        return false;
+        
+    if(_node == first){ 
+        remove_front();
+        return true;
+    }
+    if(_node == last){
+        remove_back();
+        return true;
+    } 
+    Node* current = first;
+    Node* foundNode = current->next;
+    while(current->next != _node)
+        current = current->next;
+        
+    current->next = _node->next;
 
+    delete foundNode;
+    return true;
 }
